1afd9bd69f5f6cc1e2b2352e1c727ed033acf200
Alonso Ortiz <alonsoortiz73@gmail.com>
2023-07-12 12:16:39 -0700

    Added 404 not found page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Home from "./pages/Home.js";
 import People from "./pages/People.js";
 import ContactUs from "./pages/ContactUs.js"
 import Location from "./pages/Location.js"
+import NotFound from "./pages/NotFound.js";
 import "./App.css";
 
 function App() {
@@ -16,6 +17,7 @@ function App() {
             <Route path="ContactUs" element={<ContactUs />} />
             <Route path="/People" element={<People />} />
             <Route path="/Location" element={<Location />} />
+            <Route path="*" element={<NotFound />} />
           </Routes>
         </BrowserRouter>
     </StyledEngineProvider>
---
+++ src/pages/NotFound.js
@@ -0,0 +1,40 @@
+import { Grid, Typography, Box } from "@mui/material";
+import { Helmet } from "react-helmet-async";
+import { Link } from "react-router-dom";
+import { BiSolidError } from "react-icons/bi";
+
+const NotFound = () => {
+  return (
+    <>
+      <Helmet>
+        <title>Not Found</title>
+      </Helmet>
+
+      <Box className="centered full-view">
+        <Grid
+          container
+          spacing={3}
+          display="flex"
+          alignItems="center"
+          direction="row"
+        >
+          <Grid item xs={12} className="centered" sx={{ color: "yellow" }}>
+            <BiSolidError size={100} />
+          </Grid>
+          <Grid item xs={12} className="centered">
+            <Typography variant="h4" className="errorMessage">
+              Oops! Page not found.
+            </Typography>
+          </Grid>
+          <Grid item xs={12} className="centered">
+            <Link to="/" className="go-home-link">
+              <Typography variant="h6">Go Home</Typography>
+            </Link>
+          </Grid>
+        </Grid>
+      </Box>
+    </>
+  );
+};
+
+export default NotFound;
