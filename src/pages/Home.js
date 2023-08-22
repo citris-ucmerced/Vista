d03d915804d1d8662ee8f01cd6e0a8381eca1ad1
Alonso <alonsoortiz73@gmail.com>
2023-08-21 22:39:36 -0700

    Change Boxes to Grid and improve responsivness

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import { Box, Typography } from "@mui/material";
+import { Grid, Typography } from "@mui/material";
 import { Helmet } from "react-helmet-async";
 
 import Navbar from "../components/Navbar";
@@ -19,27 +19,29 @@ const Home = () => {
 
       <div>
         <Navbar />
-        <Box className="home-banner">
-          <Box className="home-logo" xs={12}>
+        <Grid container direction="row" className="home-banner">
+          <Grid item className="home-logo" xs={12}>
             <img src={logoExtendedWhite} alt="Vista Logo White" />
-          </Box>
-          <Typography
-            className="home-subtitle"
-            component="p"
-            align="center"
-            sx={{ 
-              fontSize: "1.4rem",
-              marginInline: "3rem",
-              marginTop: "4rem",
-              maxHeight:"170px"
-            }}
-            gutterBottom
-          >
-            The Valley Institute for Sustainability, Technology & Agriculture,
-            is a fully-functioning research unit within the University of
-            California, Merced.
-          </Typography>
-        </Box>
+          </Grid>
+          <Grid item xs={12}>
+            <Typography
+              className="home-subtitle"
+              component="p"
+              align="center"
+              sx={{
+                fontSize: "1.4rem",
+                marginInline: "3rem",
+                marginTop: "4rem",
+                maxHeight:"170px"
+              }}
+              gutterBottom
+            >
+              The Valley Institute for Sustainability, Technology & Agriculture,
+              is a fully-functioning research unit within the University of
+              California, Merced.
+            </Typography>
+          </Grid>
+        </Grid>
         <Footer />
       </div>
     </>
