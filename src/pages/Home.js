8e1d25856796e7ea35dfb831ce5cfeb43c50743f
mtapia01 <michaeltapia80@yahoo.com>
2023-11-14 11:05:41 -0800

    Feat(Home.js): Adding links to other orgs at VISTA

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -12,6 +12,8 @@ import NewsCard from "../components/NewsCard.js";
 import newsCSV from "../assets/sheets/news.csv";
 
 import vistaHomeLogo from "../assets/images/VISTA-Logo-Final-PNG-Color.png";
+import f3Logo from "../assets/images/F3_Logo.png";
+import CITRIS_Logo from "../assets/images/CITRIS_Logo.png"
 
 import "./styles/Home.css"
 
@@ -79,6 +81,27 @@ const Home = () => {
             {newsCards}
         </Box>
         <div style={{display:"flex", justifyContent:"center",}}><Link to="/News" className="button"> Read More</Link></div>
+        <Typography variant="h3" component="h1" align="center" className="page-title">
+            Our Other Services
+        </Typography>
+        <br></br>
+        <Box className="news-card-container home" mb={12} >
+          <img width={"150px"} height={"150px"} src={vistaHomeLogo} alt="Vista Logo" align="left" />
+          <span> VISTA is a ...</span>
+        </Box>
+        <Box className="news-card-container home" mb={12} >
+          <img width={"220px"} height={"220px"} src={f3Logo} alt="F3 Logo" align="right" class="left" />
+          <div >
+            <span> F3 is a ...</span>
+          </div>
+        </Box>
+        <Box className="news-card-container home" mb={12} >
+          <img width={"150px"} height={"150px"} src={CITRIS_Logo} alt="Citris Logo" align="right" class="left" />
+          <div >
+            <span> Citris is a ...</span>
+          </div>
+        </Box>
+
         <Footer />
       </div>
     </>
